26b3dedffa65e6bbaf4b4ca93b5b429b1f5260ce
Eduardo56745 <lalox1697gmail.com>
2025-06-29 20:04:53 -0600

    agrego carpeta modelos

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import faiss
 import pickle
 import numpy as np
 import os
-import requests
 
 app = FastAPI()
 
@@ -14,33 +13,8 @@ def home():
     return {"mensaje": "API para hacer preguntas a tus PDFs. Usa /preguntar?q=tu_pregunta"}
 
 
-# URLs públicas de Blob Storage
-urls = {
-    "textos.pkl": "https://eduardo56745.blob.core.windows.net/eduardo56745/textos.pkl",
-    "embeddings.pkl": "https://eduardo56745.blob.core.windows.net/eduardo56745/embeddings.pkl",
-    "indice_faiss.index": "https://eduardo56745.blob.core.windows.net/eduardo56745/indice_faiss.index"
-}
-
-# Carpeta local para guardar archivos
+# Carpeta local donde ya subiste los archivos
 modelo_path = "modelos"
-os.makedirs(modelo_path, exist_ok=True)
-
-
-def descargar_archivo(nombre_archivo):
-    ruta_local = os.path.join(modelo_path, nombre_archivo)
-    if not os.path.exists(ruta_local):
-        print(f"Descargando {nombre_archivo}...")
-        r = requests.get(urls[nombre_archivo])
-        r.raise_for_status()
-        with open(ruta_local, "wb") as f:
-            f.write(r.content)
-    else:
-        print(f"{nombre_archivo} ya existe, no se descarga.")
-
-
-# Descargar archivos si no existen
-for archivo in urls.keys():
-    descargar_archivo(archivo)
 
 # Cargar modelo y datos
 modelo = SentenceTransformer('all-MiniLM-L6-v2')
